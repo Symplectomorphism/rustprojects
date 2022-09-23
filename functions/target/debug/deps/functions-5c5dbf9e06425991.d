a939ef2f007b4d19986a194f10a5766e0d6f0646
Aykut C. Satici <acsatici@gmail.com>
2022-09-23 14:30:08 -0600

    rust functions

---
+++ functions/target/debug/deps/functions-5c5dbf9e06425991.d
@@ -0,0 +1,5 @@
+/home/aykut/repos/rustprojects/functions/target/debug/deps/functions-5c5dbf9e06425991: src/main.rs
+
+/home/aykut/repos/rustprojects/functions/target/debug/deps/functions-5c5dbf9e06425991.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/aykut/repos/rustprojects/functions/target/debug/functions: /home/aykut/repos/rustprojects/functions/src/main.rs
