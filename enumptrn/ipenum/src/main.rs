e57dcd6186a82e2a54e758910b8a47ff337404e9
Aykut C. Satici <acsatici@gmail.com>
2022-09-26 09:45:40 -0600

    struct bundling enum

--- enumptrn/ipenum/src/main.rs
+++ enumptrn/ipenum/src/main.rs
@@ -3,12 +3,31 @@ enum IpAddrKind {
     V6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    route(four);
-    route(six);
+//     route(four);
+//     route(six);
+
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from ("127.0.0.1"),
+    };
+
+    let loopback = IpAddr{
+        kind: IpAddrKind::V6,
+        address: String::from ("::1"),
+    };
+
+    route(home);
+    route(loopback);
 }
 
-fn route(ip_kind: IpAddrKind) {}
+// fn route(ip_kind: IpAddrKind) {}
+fn route(ip: IpAddr) {}
