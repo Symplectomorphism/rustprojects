68babfbcb64b8fff68096a740821f079d36be4ba
Aykut C. Satici <acsatici@gmail.com>
2022-09-26 10:05:26 -0600

    make them enum with data

--- enumptrn/ipenum/src/main.rs
+++ enumptrn/ipenum/src/main.rs
@@ -1,29 +1,37 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+//     let four = IpAddrKind::V4;
+//     let six = IpAddrKind::V6;
 
 //     route(four);
 //     route(six);
 
-    let home = IpAddr{
-        kind: IpAddrKind::V4,
-        address: String::from ("127.0.0.1"),
-    };
+    // let home = IpAddr{
+    //     kind: IpAddrKind::V4,
+    //     address: String::from ("127.0.0.1"),
+    // };
 
-    let loopback = IpAddr{
-        kind: IpAddrKind::V6,
-        address: String::from ("::1"),
-    };
+    // let loopback = IpAddr{
+    //     kind: IpAddrKind::V6,
+    //     address: String::from ("::1"),
+    // };
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
 
     route(home);
     route(loopback);
