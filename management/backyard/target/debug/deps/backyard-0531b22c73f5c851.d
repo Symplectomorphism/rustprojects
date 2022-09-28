062589d4e9c863c13cec952ef5a1ff7d49cdb9db
Aykut C. Satici <acsatici@gmail.com>
2022-09-28 12:35:18 -0600

    initial module structure

---
+++ management/backyard/target/debug/deps/backyard-0531b22c73f5c851.d
@@ -0,0 +1,7 @@
+/home/aykut/repos/rustprojects/management/backyard/target/debug/deps/backyard-0531b22c73f5c851.rmeta: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+/home/aykut/repos/rustprojects/management/backyard/target/debug/deps/backyard-0531b22c73f5c851.d: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+src/main.rs:
+src/garden.rs:
+src/garden/vegetables.rs:
---
+++ management/backyard/target/debug/deps/backyard-d6c4a9bde9de6940.d
@@ -0,0 +1,7 @@
+/home/aykut/repos/rustprojects/management/backyard/target/debug/deps/backyard-d6c4a9bde9de6940: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+/home/aykut/repos/rustprojects/management/backyard/target/debug/deps/backyard-d6c4a9bde9de6940.d: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+src/main.rs:
+src/garden.rs:
+src/garden/vegetables.rs:
