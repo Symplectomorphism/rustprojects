b6d176abac89197264cc9f41c1f06d89e222174f
Aykut C. Satici <acsatici@gmail.com>
2022-09-29 17:58:58 -0600

    chapter 8 is done.

--- collections/hashmaps/src/main.rs
+++ collections/hashmaps/src/main.rs
@@ -1,3 +1,43 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+//     scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point.
+
+//     println!("Field name is {}.", field_name);
+    
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(60);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
