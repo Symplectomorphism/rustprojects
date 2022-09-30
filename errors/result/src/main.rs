9a75572b4eb4fe3225b808a1d64c500a1251eba0
Aykut C. Satici <acsatici@gmail.com>
2022-09-29 19:03:36 -0600

    propagating

--- errors/result/src/main.rs
+++ errors/result/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project.");
 }
