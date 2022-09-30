016825c8402ddca62382fdea726cd1f9b22d0965
Aykut C. Satici <acsatici@gmail.com>
2022-09-29 19:36:08 -0600

    error propagation

--- errors/propagating/src/main.rs
+++ errors/propagating/src/main.rs
@@ -1,5 +1,8 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
+// use std::io;
+// use std::fs;
 
 fn read_username_from_file() -> Result<String, io::Error> {
     let username_file_result = File::open("hello.txt");
@@ -17,6 +20,28 @@ fn read_username_from_file() -> Result<String, io::Error> {
     }
 }
 
-fn main() {
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file4() -> Result<String, io::Error> {
+    std::fs::read_to_string("hello.txt")
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    let greeting_file = File::open("hello.txt")?;
+
+    Ok(())
 }
