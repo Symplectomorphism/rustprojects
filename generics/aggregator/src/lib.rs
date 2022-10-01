b62a570437696c59c480d1fd518002462c014d3c
Aykut C. Satici <acsatici@gmail.com>
2022-09-30 21:21:25 -0600

    default traits

---
+++ generics/aggregator/src/lib.rs
@@ -0,0 +1,57 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self. author, self.location)
+    // }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
