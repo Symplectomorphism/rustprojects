f3ecdbb49cf1228894b53ee398c68d1da2f21655
Aykut C. Satici <acsatici@gmail.com>
2022-10-02 11:16:04 -0600

    modularize minigrep

--- io/minigrep/src/main.rs
+++ io/minigrep/src/main.rs
@@ -1,11 +1,22 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
