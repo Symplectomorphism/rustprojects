8050283dcebb09edff8287cafc1bc7395d423497
Aykut C. Satici <acsatici@gmail.com>
2022-10-01 23:03:40 -0600

    more practicing the test stuff

--- tests/adder/src/lib.rs
+++ tests/adder/src/lib.rs
@@ -1,3 +1,20 @@
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -11,7 +28,16 @@ impl Rectangle {
 }
 
 pub fn add(left: usize, right: usize) -> usize {
-    left + right
+    left + right 
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+//     format!("Hello!")
 }
 
 #[cfg(test)]
@@ -55,5 +81,36 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
