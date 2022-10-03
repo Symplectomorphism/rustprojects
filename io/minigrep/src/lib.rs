d054150206c16d6e139c988f0ab11bf221f64fd0
Aykut C. Satici <acsatici@gmail.com>
2022-10-02 19:19:17 -0600

    end of chapter 13.

--- io/minigrep/src/lib.rs
+++ io/minigrep/src/lib.rs
@@ -20,17 +20,41 @@ impl Config {
      * }
      */
     
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    // pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    //     if args.len() < 3 {
+    //         return Err("not enough arguments");
+    //     }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    //     let query = args[1].clone();
+    //     let file_path = args[2].clone();
+
+    //     let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+    //     Ok(Config { query, file_path, ignore_case })
+    // }
+
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+        ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string."),
+        };
+        
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path."),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -50,33 +74,53 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// pub fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+//     let mut results = Vec::new();
+// 
+//     for line in contents.lines() {
+//         if line.contains(query) {
+//             results.push(line);
+//         }
+//     }
+// 
+//     results
+// }
+
 pub fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
+
+// pub fn search_case_insensitive<'a>(
+//     query: &str, 
+//     contents:&'a str
+// ) -> Vec<&'a str> {
+//     let query = query.to_lowercase();
+//     let mut results = Vec::new();
+// 
+//     for line in contents.lines() {
+//         if line.to_lowercase().contains(&query) {
+//             results.push(line);
+//         }
+//     }
+// 
+//     results
+// }
+
 
 pub fn search_case_insensitive<'a>(
     query: &str, 
     contents:&'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
+
 
 #[cfg(test)]
 mod tests {
