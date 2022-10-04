d37705b9488d8f6833fe7ee8be4428ee541fa750
Aykut C. Satici <acsatici@gmail.com>
2022-10-04 10:55:44 -0600

    concurrent message passing

--- concurrency/simultaneous/src/main.rs
+++ concurrency/simultaneous/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +13,14 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    // drop(v);
+
+    handle2.join().unwrap();
 }
