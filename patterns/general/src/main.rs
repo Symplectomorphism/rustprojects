63addea80bb8d2e070d480bc6ef1b495efd0fd45
Aykut C. Satici <acsatici@gmail.com>
2022-10-05 21:01:50 -0600

    patterns in general

---
+++ patterns/general/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background.");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color.");
+        } else {
+            println!("Using orange as the background color.");
+        }
+    } else {
+        println!("Using blue as the background color.");
+    }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}.", value, index);
+    }
+
+    let point = (3, 5, 8);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y, z): &(i32, i32, i32)) {
+    println!("Current location: ({}, {}, {})", x, y, z);
+}
