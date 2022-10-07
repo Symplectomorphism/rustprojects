ec2dc1956259c5e13fb7d1d1eb37381d5680b1ae
Aykut C. Satici <acsatici@gmail.com>
2022-10-06 18:49:36 -0600

    looking

--- advanced/notsafe/src/main.rs
+++ advanced/notsafe/src/main.rs
@@ -3,4 +3,27 @@ fn main() {
 
     let r1 = &num as *const i32;
     let r2 = &mut num as *mut i32;
+
+    let address = 0x012345usize;
+    let _r = address as *const i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }
+
+unsafe fn dangerous () {}
