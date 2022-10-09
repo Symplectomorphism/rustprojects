58829ab554e1e4d8b4ed01c9eec58faa7c77b35d
Aykut C. Satici <acsatici@gmail.com>
2022-10-09 02:51:49 -0600

    enums down

--- rustlings/exercises/enums/enums3.rs
+++ rustlings/exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,16 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor((x, y, z)) => {
+                self.color.0 = x;
+                self.color.1 = y;
+                self.color.2 = z;
+            },
+            Message::Move(p) => { self.position = p },
+            Message::Quit => { self.quit = true },
+            _ => (),
+        }
     }
 }
 
